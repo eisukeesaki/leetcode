78ce54aae3124f25e1a710a4718d2a5e37779502
Eisuke Esaki <eisukeesaki@gmail.com>
2023-02-07 00:28:02 -0800

    implemented alternate solutions

--- Running_Sum_of_1d_Array/Running_Sum_of_1d_Array.ts
+++ Running_Sum_of_1d_Array/Running_Sum_of_1d_Array.ts
@@ -1,12 +1,38 @@
-function getRunningSum(nums: number[]): number[] {
-  let runningSum = nums;
+/**
+ * fast & low memory usage
+ */
+// function getRunningSum(nums: number[]): number[] {
+//   let runningSum = nums;
 
-  for (let i = 1; i < runningSum.length; i++) {
-    runningSum[i] = nums[i - 1] + nums[i];
+//   for (let i = 1; i < runningSum.length; i++) {
+//     runningSum[i] = nums[i - 1] + nums[i];
+//   }
+
+//   return runningSum;
+// }
+
+/**
+ * readable
+ */
+function getRunningSum(nums: number[]): number[] {
+  const rSum = new Array(nums.length);
+
+  rSum[0] = nums[0];
+  for (let i = 1; i < rSum.length; i++) {
+    rSum[i] = rSum[i - 1] + nums[i];
   }
 
-  return runningSum;
+  return rSum;
 }
+
+/**
+ * clever
+ */
+// function getRunningSum(nums: number[]): number[] {
+//   let sum: number = 0;
+
+//   return nums.map(element => sum += element);
+// }
 
 /** expected input:output
  * input:  [1,2,3,4]
@@ -24,7 +50,7 @@ function getRunningSum(nums: number[]): number[] {
  * input:  [3,1,2,10,1]
  * output: [3,4,6,16,17]
  */
-const input: Array<number> = [3,1,2,10,1];
+const input: Array<number> = [3, 1, 2, 10, 1];
 
 const runningSum: Array<number> = getRunningSum(input);
 
